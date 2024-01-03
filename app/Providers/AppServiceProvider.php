9cbee67efbf428fc4892381fc609e1a054bedadf
kotelnikov.a_qschool.newgrade <ko7elnikoval@yandex.ru>
2024-01-03 21:44:09 +0300

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // данные два метода работают пока проект в разработке (2я строка сработает если забыть прописать в модели поля $fillable)
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+
+        //если какой-то запрос к бд дольше чем 500 сек, то идет отправка в телегу (пока не реализовано)
+        DB::whenQueryingForLongerThan(500, function (Connection $connection){
+
+        });
+    }
+}
